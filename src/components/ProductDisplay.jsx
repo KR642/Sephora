5fa9591bfe170f1b30e683ec5dedb21e62408591
KR642 <KR642@live.mdx.ac.uk.com>
2024-02-11 23:07:08 +0000

    3d carousel done

--- src/components/ProductDisplay.jsx
+++ src/components/ProductDisplay.jsx
@@ -8,11 +8,11 @@ import 'swiper/css/navigation';
 import Product from './Product';
 import car1 from '../assets/car-1.jpg';
 import { EffectCoverflow, Pagination, Navigation } from 'swiper';
-
+import { MdOutlineArrowBackIosNew } from "react-icons/md";
+import { MdOutlineArrowForwardIos } from "react-icons/md";
 const ProductDisplay = () => {
     return (
         <div className="container">
-            <h1 className="heading">Flower Gallery</h1>
                 <Swiper
                     effect={'coverflow'}
                     grabCursor={true}
@@ -25,7 +25,7 @@ const ProductDisplay = () => {
                     depth: 100,
                     modifier: 2.5,
                     }}
-                    pagination={{ el: '.swiper-pagination', clickable: true }}
+                   
                     navigation={{
                     nextEl: '.swiper-button-next',
                     prevEl: '.swiper-button-prev',
@@ -55,12 +55,24 @@ const ProductDisplay = () => {
                 <SwiperSlide>
                     <img src={car1} alt="" />
                 </SwiperSlide>
+                <SwiperSlide>
+                    <img src={car1} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={car1} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={car1} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={car1} alt="" />
+                </SwiperSlide>
             <div className="slider-controler">
             <div className="swiper-button-prev slider-arrow">
-            <ion-icon name="arrow-back-outline"></ion-icon>
+                <MdOutlineArrowBackIosNew/>
             </div>
             <div className="swiper-button-next slider-arrow">
-            <ion-icon name="arrow-forward-outline"></ion-icon>
+                    <MdOutlineArrowForwardIos/>
             </div>
             <div className="swiper-pagination"></div>
             </div>
